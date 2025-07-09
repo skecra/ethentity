f87d3d397db04de21aa85c1b1045f158689978b6
unknown <lukaradulovic1998@gmail.com>
2025-07-09 14:47:17 +0200

    komit 2

--- test/Ethentity.js
+++ test/Ethentity.js
@@ -1,20 +1,15 @@
 const { expect } = require("chai")
-const { ethers } = require("hardhat")
-const { describe } = require("mocha")
-const { transpileModule } = require("typescript")
 
 const tokens = (n) => {
   return ethers.utils.parseUnits(n.toString(), 'ether')
 }
 
 describe("Ethentity", () => {
-
-
   let ethentity
   let deployer, owner1
 
-  const NAME = "ETH Daddy"
-  const SYMBOL = "ETHD"
+  const NAME = "Ethentity"
+  const SYMBOL = "EID"
 
   beforeEach(async () => {
     // Setup accounts
@@ -25,11 +20,36 @@ describe("Ethentity", () => {
     ethentity = await Ethentity.deploy(NAME, SYMBOL)
 
     // List a domain
-    const transaction = await ethentity.connect(deployer).list("luka.eth", tokens(10))
+    const transaction = await ethentity.connect(deployer).list("jack.eth", tokens(10))
     await transaction.wait()
   })
-  })
 
+  describe("Deployment", () => {
+    it("Sets the name", async () => {
+      const result = await ethentity.name()
+      expect(result).to.equal(NAME)
+    })
+
+    it("Sets the symbol", async () => {
+      const result = await ethentity.symbol()
+      expect(result).to.equal(SYMBOL)
+    })
+
+    it("Sets the owner", async () => {
+      const result = await ethentity.owner()
+      expect(result).to.equal(deployer.address)
+    })
+
+    it("Returns the max supply", async () => {
+      const result = await ethentity.maxSupply()
+      expect(result).to.equal(1)
+    })
+
+    it("Returns the total supply", async () => {
+      const result = await ethentity.totalSupply()
+      expect(result).to.equal(0)
+    })
+  })
 
   describe("Domain", () => {
     it('Returns domain attributes', async () => {
@@ -40,5 +60,54 @@ describe("Ethentity", () => {
     })
   })
 
+  describe("Minting", () => {
+    const ID = 1
+    const AMOUNT = ethers.utils.parseUnits("10", 'ether')
 
+    beforeEach(async () => {
+      const transaction = await ethentity.connect(owner1).mint(ID, { value: AMOUNT })
+      await transaction.wait()
+    })
 
+    it('Updates the owner', async () => {
+      const owner = await ethentity.ownerOf(ID)
+      expect(owner).to.be.equal(owner1.address)
+    })
+
+    it('Updates the domain status', async () => {
+      const domain = await ethentity.getDomain(ID)
+      expect(domain.isOwned).to.be.equal(true)
+    })
+
+    it('Updates the contract balance', async () => {
+      const result = await ethentity.getBalance()
+      expect(result).to.be.equal(AMOUNT)
+    })
+  })
+
+  describe("Withdrawing", () => {
+    const ID = 1
+    const AMOUNT = ethers.utils.parseUnits("10", 'ether')
+    let balanceBefore
+
+    beforeEach(async () => {
+      balanceBefore = await ethers.provider.getBalance(deployer.address)
+
+      let transaction = await ethentity.connect(owner1).mint(ID, { value: AMOUNT })
+      await transaction.wait()
+
+      transaction = await ethentity.connect(deployer).withdraw()
+      await transaction.wait()
+    })
+
+    it('Updates the owner balance', async () => {
+      const balanceAfter = await ethers.provider.getBalance(deployer.address)
+      expect(balanceAfter).to.be.greaterThan(balanceBefore)
+    })
+
+    it('Updates the contract balance', async () => {
+      const result = await ethentity.getBalance()
+      expect(result).to.equal(0)
+    })
+  })
+})
